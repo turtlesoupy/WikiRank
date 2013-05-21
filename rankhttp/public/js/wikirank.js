74528534fceb38652b778210b8c196c3f946ca45
Thomas Dimson <tdimson@gmail.com>
2013-05-21 00:18:37 -0700

    Okay, it works!

--- rankhttp/public/js/wikirank.js
+++ rankhttp/public/js/wikirank.js
@@ -1,6 +1,22 @@
 "use strict";
 
 $(document).ready(function() {
+  var spinner = new Spinner({
+    lines: 15,
+    length: 0,
+    width: 12,
+    radius: 30
+  });
+
+  function startSpin() {  
+    spinner.spin($(".spinner").get(0)); 
+    $(".influenceLoader").show();
+  }
+  function endSpin() {  
+    spinner.stop(); 
+    $(".influenceLoader").hide();
+  }
+
   $("input.first-object, input.second-object").typeahead({
     name: "objects",
     remote: {
@@ -11,5 +27,55 @@ $(document).ready(function() {
         return suggestions;
       }
     }
+  }).bind("typeahead:selected", function(e) {
+    var otherSelector = "input.second-object";
+    if($(this).hasClass("second-object")) {
+      otherSelector = "input.first-object";
+    }
+    
+    if($(otherSelector).val() === "") {
+      return; 
+    }
+
+    var things = [
+      $(otherSelector).val(),
+      $(this).val()
+    ];
+
+    startSpin();
+    $(".influenceResults").hide();
+    $.getJSON("/compare", {
+      "things": things
+    })
+      .done(function(data) {
+        endSpin();
+
+        var ratio = data.pages[0].Rank / data.pages[1].Rank;
+        var influenceText, copy;
+        console.log(ratio);
+
+        if(ratio >= 0.95 && ratio <= 1.05) {
+          influenceText = "about as";
+          copy = "influential as"
+        } else {
+          var textRatio;
+          if(ratio < 10) {
+            textRatio = Number(ratio.toPrecision(2));
+          } else {
+            textRatio = Number(ratio.toPrecision(1));
+          }
+          influenceText = textRatio + (ratio >= 1.0 ?  "x more" : "x less");
+          copy = "influential than";
+        }
+
+        $(".influencer.influencerFirst").html("<strong>" + data.pages[0].Title + "</strong> is");
+        $(".influenceResultsRatio").text(influenceText);
+        $(".influencer.influencerSecond").html(copy + " <strong>" + data.pages[1].Title + "</strong>");
+        $(".influenceResults").show();
+
+      })
+      .fail(function(xhr, textStatus, errorThrown) {
+        endSpin();
+      });
   });
 });
--- rankhttp/server.go
+++ rankhttp/server.go
@@ -36,13 +36,12 @@ func loadTemplate(templateName string) (engine *gohaml.Engine, err error) {
   return gohaml.NewEngine(bb.String())
 }
 
-var namedEntityRegex = regexp.MustCompile("/named_entity_suggestions([?].*)?")
 
 func index(w http.ResponseWriter, r *http.Request) {
   var scope = make(map[string]interface{})
   engine, err := loadTemplate("index.haml")
   if err != nil {
-    // TODO: something
+    http.Error(w, err.Error(), http.StatusInternalServerError)
   }
 
   response := engine.Render(scope)
@@ -51,25 +50,61 @@ func index(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, response)
 }
 
+func compareThings(autocompleteIndex *ranklib.Trie, w http.ResponseWriter, r *http.Request) {
+  things, ok := r.URL.Query()["things[]"]
+  if !ok || len(things) != 2 {
+    http.Error(w, "Bad search", http.StatusBadRequest)
+    return
+  }
+  log.Printf("Comparing %q", things)
+
+
+  page1, ok := autocompleteIndex.GetEntry(ranklib.NormalizeSuggestionPrefix(things[0]))
+  if !ok {
+    http.Error(w, fmt.Sprintf("%s doesn't exist", things[0]), http.StatusBadRequest)
+    return
+  }
+
+  page2, ok := autocompleteIndex.GetEntry(ranklib.NormalizeSuggestionPrefix(things[1]))
+  if !ok {
+    http.Error(w, fmt.Sprintf("%s doesn't exist", things[1]), http.StatusBadRequest)
+    return
+  }
+
+  responseObject, err := json.Marshal(map[string]interface{}{
+    "pages": []interface{}{page1, page2},
+  })
+
+  if err != nil {
+    http.Error(w, "Unable to jsonify comparison", http.StatusInternalServerError)
+    return
+  }
+
+  response := string(responseObject)
+  w.Header().Set("Content-Length", strconv.Itoa(len(response)))
+  w.Header().Set("Content-Type", "application/json; charset=utf-8")
+  fmt.Fprintf(w, response)
+}
 
 func namedEntitySuggestions(autocompleteIndex *ranklib.Trie, w http.ResponseWriter, r *http.Request) {
   suggestions := map[string]interface{}{}
   search, ok := r.URL.Query()["q"]
   log.Printf("Search is %s from URL %s", search, r.URL)
   if !ok || len(search) != 1 {
-    //TODO: something
     http.Error(w, "Bad search prefix", http.StatusBadRequest)
     return
   }
 
   suggestions["suggestions"], ok = autocompleteIndex.GetTopSuggestions(ranklib.NormalizeSuggestionPrefix(search[0]), 6)
   if !ok {
-    //TODO: something
+    http.Error(w, "Bad trie traversal", http.StatusInternalServerError)
+    return
   }
 
   responseObject, err := json.Marshal(suggestions)
   if err != nil {
-    // TODO: something
+    http.Error(w, "Unable to jsonify suggestions", http.StatusInternalServerError)
+    return
   }
   response := string(responseObject)
   w.Header().Set("Content-Length", strconv.Itoa(len(response)))
@@ -93,11 +128,15 @@ func setupStatics(rootStatics []string, exposedStaticDirs []string) {
   log.Printf("Static routes are set")
 }
 
+var namedEntityRegex = regexp.MustCompile("/named_entity_suggestions([?].*)?")
+var compareRegex = regexp.MustCompile("/compare([?].*)?")
 func Serve(autocompleteIndex *ranklib.Trie, port int) {
   log.Printf("Server: running on port %d", port)
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     if namedEntityRegex.MatchString(r.URL.Path) {
       namedEntitySuggestions(autocompleteIndex, w, r)
+    } else if compareRegex.MatchString(r.URL.Path) {
+      compareThings(autocompleteIndex, w, r)
     } else {
       index(w, r)
     }
